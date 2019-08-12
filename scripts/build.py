7d569cedd0b79aa8562400d8fc1996234574f26c
PommeDroid <jacfet@protonmail.com>
2019-08-12 16:05:14 +0200

    Refactor setup.py/build.py for reuse on DreamTime

--- scripts/build.py
+++ scripts/build.py
@@ -1,70 +1,88 @@
 import argparse
+import importlib
 import logging
-
+import os
 import subprocess
-import shutil
-
 import sys
 
-from _common import get_os, setup_log, OS, get_python_version, cd, check_pyinstaller
-
-parser = argparse.ArgumentParser(description='cli builder')
-parser.add_argument('-d', '--debug', action='store_true',
-                    help='Set log level to Debug')
-
-args = parser.parse_args()
-
-log = setup_log(logging.DEBUG if args.debug else logging.INFO)
-
-## System & Dependencies Check
-detected_os = get_os()
-detected_py = get_python_version()
-log.debug("OS : {}".format(detected_os))
-log.debug("Python version : {}".format(detected_py))
-
-if detected_os == OS.UNKNOWN:
-    log.fatal("Unknown OS !")
-    exit(1)
-
-if detected_py < (3, 5):
-    log.fatal("Unsupported python version !")
-    exit(1)
-
-if not check_pyinstaller():
-    log.fatal("Pyinstaller is not install. It's a required dependency !")
-    exit(1)
+spec = importlib.util.spec_from_file_location("_common",
+                                              os.path.join(os.path.dirname(os.path.abspath(__file__)), "./_common.py"))
+c = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(c)
 
 
-## Build Cli
-def pyinstaller_args():
-    pyinstaller_args = [
-        '--workpath=./build/',
-        '--specpath=.',
-        '-y',
-        '--onedir',
-        '--name=cli',
-        'main.py',
-    ]
-    if detected_os == OS.LINUX:
-        return pyinstaller_args
-    if detected_os == OS.MAC:
-        return pyinstaller_args
-    if detected_os == OS.WIN:
-        pyinstaller_args.extend(['--add-binary=./third/msvcp/msvcp140.dll;.'])
-        return pyinstaller_args
+def add_arg_parser(parser):
+    parser.add_argument('-d', '--debug', action='store_true',
+                        help='Set log level to Debug')
 
 
-log.info('Building Cli')
-with cd(".."):
-    cmd = [sys.executable, '-m', 'PyInstaller'] + pyinstaller_args()
-    log.debug(cmd)
-    r = subprocess.run(cmd)
-    if r.returncode != 0:
-        log.fatal("Cli build failed")
+def check_dependencies():
+    ## System & Dependencies Check
+    c.log.debug("OS : {}".format(c.get_os()))
+    c.log.debug("Python version : {}".format(c.get_python_version()))
+
+    if c.get_os() == c.OS.UNKNOWN:
+        c.log.fatal("Unknown OS !")
         exit(1)
-log.info('Cli successfully built')
 
-log.info('Build completed!')
-log.info(
-    'It should have generated a folder called dist/, inside you will find the final project files that you can share with everyone!')
-log.info('Enjoy and remember to respect the License!')
+    if c.get_python_version() < (3, 5):
+        c.log.fatal("Unsupported python version !")
+        exit(1)
+
+    if not c.check_pyinstaller():
+        c.log.fatal("Pyinstaller is not install. It's a required dependency !")
+        exit(1)
+
+
+def cli_build(args, dist_path="../dist"):
+    def pyinstaller_args():
+        pyinstaller_args = [
+            '--workpath=./build/',
+            '--specpath=.',
+            '-y',
+            '--onedir',
+            '--name=dreampower',
+            '--distpath={}'.format(dist_path),
+            'main.py',
+        ]
+        if c.get_os() == c.OS.LINUX:
+            return pyinstaller_args
+        if c.get_os() == c.OS.MAC:
+            return pyinstaller_args
+        if c.get_os() == c.OS.WIN:
+            pyinstaller_args.extend(['--add-binary=./third/msvcp/msvcp140.dll;.'])
+            return pyinstaller_args
+
+    c.log.info('Building Cli')
+    with c.cd(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")):
+        cmd = [sys.executable, '-m', 'PyInstaller'] + pyinstaller_args()
+        c.log.debug(cmd)
+        r = subprocess.run(cmd)
+        if r.returncode != 0:
+            c.log.fatal("Cli build failed")
+            exit(1)
+    c.log.info('Cli successfully built')
+
+
+def run(args, dist_path="../dist"):
+    cli_build(args, dist_path)
+
+    c.log.info('Build completed!')
+    c.log.info(
+        'It should have generated a folder called dist/, inside you will find '
+        'the final project files that you can share with everyone!')
+    c.log.info('Enjoy and remember to respect the License!')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='cli builder')
+    add_arg_parser(parser)
+    args = parser.parse_args()
+
+    check_dependencies()
+
+    if args.debug:
+        c.log.setLevel(logging.DEBUG)
+
+    ## Build Cli
+    run(args)
--- scripts/setup.py
+++ scripts/setup.py
@@ -1,89 +1,115 @@
 import argparse
 import fileinput
+import importlib
 import logging
-
-import subprocess
-
 import os
-
+import subprocess
 import sys
-from _common import get_os, setup_log, OS, create_temporary_copy, get_python_version, cd
 
-parser = argparse.ArgumentParser(description='cli dependencies setup')
-parser.add_argument('-d', '--debug', action='store_true',
-                    help='Set log level to Debug')
-parser.add_argument('-np', '--no_pyinstaller', action='store_true',
-                    help='Don\'t install pyinstaller')
-parser.add_argument('--cpu', action='store_true',
-                    help='No cuda support')
-parser.add_argument('-pnc', '--pip_no_cache_dir', action='store_true',
-                    help='Use --no_cache_dir for pip commands')
-parser.add_argument('-pu', '--pip_user', action='store_true',
-                    help='Use --user for pip commands')
-args = parser.parse_args()
-
-log = setup_log(logging.DEBUG if args.debug else logging.INFO)
-
-## System & Dependencies Check
-detected_os = get_os()
-detected_py = get_python_version()
-log.debug("OS : {}".format(detected_os))
-log.debug("Python version : {}".format(detected_py))
-
-if detected_os == OS.UNKNOWN:
-    log.fatal("Unknown OS !")
-    exit(1)
-
-if detected_py < (3, 5):
-    log.fatal("Unsupported python version !")
-    exit(1)
-
-## Cli dependencies
-pip_commands_extend = (['--user'] if args.pip_user else []) + (['--no-cache-dir'] if args.pip_no_cache_dir else [])
+spec = importlib.util.spec_from_file_location("_common",
+                                              os.path.join(os.path.dirname(os.path.abspath(__file__)), "./_common.py"))
+c = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(c)
 
 
-## Pyinstaller
-if not args.no_pyinstaller:
-    log.info('Installing pyinstaller')
+def add_arg_parser(parser):
+    parser.add_argument('-d', '--debug', action='store_true',
+                        help='Set log level to Debug')
+    parser.add_argument('-np', '--no_pyinstaller', action='store_true',
+                        help='Don\'t install pyinstaller')
+    parser.add_argument('--cpu', action='store_true',
+                        help='No cuda support')
+    parser.add_argument('-pnc', '--pip_no_cache_dir', action='store_true',
+                        help='Use --no_cache_dir for pip commands')
+    parser.add_argument('-pu', '--pip_user', action='store_true',
+                        help='Use --user for pip commands')
+
+
+def check_dependencies():
+    c.log.debug("OS : {}".format(c.get_os()))
+    c.log.debug("Python version : {}".format(c.get_python_version()))
+
+    if c.get_os() == c.OS.UNKNOWN:
+        c.log.fatal("Unknown OS !")
+        exit(1)
+
+    if c.get_python_version() < (3, 5):
+        c.log.fatal("Unsupported python version !")
+        exit(1)
+
+
+def pyinstaller(args, pip_commands_extend=None):
+    if pip_commands_extend is None:
+        pip_commands_extend = []
+
+    c.log.info('Installing pyinstaller')
     r = subprocess.run([sys.executable, '-m', 'pip', 'install', 'pyinstaller'] + pip_commands_extend)
     if r.returncode != 0:
-        log.fatal("Pyinstaller installation failed")
+        c.log.fatal("Pyinstaller installation failed")
         exit(1)
-    log.info('Pyinstaller successfully installed')
+    c.log.info('Pyinstaller successfully installed')
 
 
-def torch_version():
-    if args.cpu:
-        if detected_os == OS.LINUX:
-            return "https://download.pytorch.org/whl/cpu/torch-1.1.0-cp{0}{1}-cp{0}{1}m-linux_x86_64.whl".format(
-                *get_python_version())
-        if detected_os == OS.MAC:
-            return "torch"
-        if detected_os == OS.WIN:
-            return "https://download.pytorch.org/whl/cpu/torch-1.1.0-cp{0}{1}-cp{0}{1}m-win_amd64.whl".format(
-                *get_python_version())
-    else:
-        if detected_os == OS.LINUX:
-            return "https://download.pytorch.org/whl/cu100/torch-1.1.0-cp{0}{1}-cp{0}{1}m-linux_x86_64.whl".format(
-                *get_python_version())
-        if detected_os == OS.MAC:
-            log.warning(
-                "# MacOS Binaries dont support CUDA, install from source if CUDA is needed. This script will install "
-                "the cpu version.")
-            return "torch"
-        if detected_os == OS.WIN:
-            return "https://download.pytorch.org/whl/cu100/torch-1.1.0-cp{0}{1}-cp{0}{1}m-win_amd64.whl".format(
-                *get_python_version())
+def cli_setup(args, pip_commands_extend=None):
+    if pip_commands_extend is None:
+        pip_commands_extend = []
+
+    def torch_version():
+        if args.cpu:
+            if c.get_os() == c.OS.LINUX:
+                return "https://download.pytorch.org/whl/cpu/torch-1.1.0-cp{0}{1}-cp{0}{1}m-linux_x86_64.whl".format(
+                    *c.get_python_version())
+            if c.get_os() == c.OS.MAC:
+                return "torch"
+            if c.get_os() == c.OS.WIN:
+                return "https://download.pytorch.org/whl/cpu/torch-1.1.0-cp{0}{1}-cp{0}{1}m-win_amd64.whl".format(
+                    *c.get_python_version())
+        else:
+            if c.get_os() == c.OS.LINUX:
+                return "https://download.pytorch.org/whl/cu100/torch-1.1.0-cp{0}{1}-cp{0}{1}m-linux_x86_64.whl".format(
+                    *c.get_python_version())
+            if c.get_os() == c.OS.MAC:
+                c.log.warning(
+                    "# MacOS Binaries dont support CUDA, install from source if CUDA is needed. "
+                    "This script will install the cpu version.")
+                return "torch"
+            if c.get_os() == c.OS.WIN:
+                return "https://download.pytorch.org/whl/cu100/torch-1.1.0-cp{0}{1}-cp{0}{1}m-win_amd64.whl".format(
+                    *c.get_python_version())
+
+    c.log.info('Installing Cli dependencies')
+    path = c.create_temporary_copy(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "../requirements.txt"), "cli-requirements.txt")
+    with fileinput.FileInput(path, inplace=True) as f:
+        for l in f:
+            print(l.replace("torch==1.1.0", torch_version()), end='')
+    r = subprocess.run([sys.executable, '-m', 'pip', 'install', '-r', path] + pip_commands_extend)
+    os.remove(path)
+    if r.returncode != 0:
+        c.log.fatal("Cli dependencies installation failed")
+        exit(1)
+    c.log.info('Cli dependencies successfully installed')
 
 
-log.info('Installing Cli dependencies')
-path = create_temporary_copy("../requirements-generic.txt", "cli-requirements.txt")
-with fileinput.FileInput(path, inplace=True) as f:
-    for l in f:
-        print(l.replace("torch==1.1.0", torch_version()), end='')
-r = subprocess.run([sys.executable, '-m', 'pip', 'install', '-r', path] + pip_commands_extend)
-os.remove(path)
-if r.returncode != 0:
-    log.fatal("Cli dependencies installation failed")
-    exit(1)
-log.info('Cli dependencies successfully installed')
+def run(args):
+    ## System & Dependencies Check
+    check_dependencies()
+
+    if args.debug:
+        c.log.setLevel(logging.DEBUG)
+
+    ## Cli dependencies
+    pip_commands_extend = (['--user'] if args.pip_user else []) + (['--no-cache-dir'] if args.pip_no_cache_dir else [])
+
+    ## Pyinstaller
+    if not args.no_pyinstaller:
+        pyinstaller(pip_commands_extend)
+
+    cli_setup(args, pip_commands_extend)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='cli dependencies setup')
+    add_arg_parser(parser)
+    args = parser.parse_args()
+    run(args)
