22847d48514c1bc0c1145cc568dfb22c89f204bf
zz3434 <zz3434@github.com>
2020-05-17 06:30:52 +0000

    fix call to FolderImageProcessing

--- daemon.py
+++ daemon.py
@@ -90,7 +90,7 @@ class Handler(FileSystemEventHandler):
                     "output": self.__out_dir,
                 })
 
-                FolderImageProcessing(args=args).run()
+                FolderImageProcessing().run(args)
 
                 Conf.log.info("Execution of {} Folder Done in {}.".format(
                     os.path.dirname(event.src_path), round(time.time() - start, 2)
